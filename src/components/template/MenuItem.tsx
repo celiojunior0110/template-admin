3ba7210cf493675248006db58d5ff7620535a7bb
Célio Júnior <celiojunior0110@gmail.com>
2023-08-09 17:51:31 -0300

    Changes in sidebar

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -1,18 +1,35 @@
-import Link from "next/link"
+'use client'
+import Link from 'next/link'
 
 type MenuItemProps = {
-  url: string
   texto: string
   icone: any
+  url?: string
+  className?: string
+  onClick?: (event: any) => void
 }
 
 export default function MenuItem(props: MenuItemProps) {
+  const url = props.url ? props.url : ''
+  const classLink = `flex flex-col justify-center items-center w-20 h-20 text-gray-600 ${props.className}`
+
   return (
-    <li className="hover:bg-gray-100">
-      <Link href={props.url} className="flex flex-col justify-center items-center w-20 h-20">
-        {props.icone}
-        <span className="text-xs font-light text-gray-600">{props.texto}</span>
-      </Link>
+    <li className="hover:bg-gray-100 cursor-pointer" onClick={props.onClick}>
+      {props.url ? (
+        <Link href={url} className={classLink}>
+          {props.icone}
+          <span className="text-xs font-light ">
+            {props.texto}
+          </span>
+        </Link>
+      ) : (
+        <a className={classLink}>
+          {props.icone}
+          <span className="text-xs font-light">
+            {props.texto}
+          </span>
+        </a>
+      )}
     </li>
   )
 }
--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,17 +1,26 @@
-import { IconeAjustes, IconeCasa, IconeSino } from '../icons'
+'use client'
+import { IconeAjustes, IconeCasa, IconeSair, IconeSino } from '../icons'
 import Logo from './Logo'
 import MenuItem from './MenuItem'
 
 export default function MenuLateral() {
   return (
-    <aside>
+    <aside className="flex flex-col">
       <div className="w-20 h-20 bg-gradient-to-r from-indigo-500 to-purple-800 flex flex-col items-center justify-center">
         <Logo />
       </div>
-      <ul>
+      <ul className="flex-grow">
         <MenuItem url="/" texto="Início" icone={IconeCasa} />
         <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
         <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino} />
+      </ul>
+      <ul className="">
+        <MenuItem
+          texto="Sair"
+          icone={IconeSair}
+          onClick={() => console.log('Logout')}
+          className="text-red-600 hover:bg-red-400 hover:text-white"
+        />
       </ul>
     </aside>
   )
