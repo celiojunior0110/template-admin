d5fbe2d3974d231ecd30a6c10143c490b164c2ee
Célio Júnior <celiojunior0110@gmail.com>
2023-08-09 16:21:52 -0300

    Add Logo component

--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,13 +1,17 @@
-import { IconeAjustes, IconeCasa, IconeSino } from "../icons";
-import MenuItem from "./MenuItem";
+import { IconeAjustes, IconeCasa, IconeSino } from '../icons'
+import Logo from './Logo'
+import MenuItem from './MenuItem'
 
 export default function MenuLateral() {
   return (
     <aside>
+      <div className="w-20 h-20 bg-gradient-to-r from-indigo-500 to-purple-800 flex flex-col items-center justify-center">
+        <Logo />
+      </div>
       <ul>
-        <MenuItem url='/' texto="Início" icone={IconeCasa} />
-        <MenuItem url='/ajustes' texto="Ajustes" icone={IconeAjustes} />
-        <MenuItem url='/notificacoes' texto="Notificações" icone={IconeSino} />
+        <MenuItem url="/" texto="Início" icone={IconeCasa} />
+        <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
+        <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino} />
       </ul>
     </aside>
   )
