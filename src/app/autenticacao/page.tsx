351ad10d3d6558ee4f3cebebd1d44b6626ca495f
Célio Júnior <celiojunior0110@gmail.com>
2023-08-10 19:57:44 -0300

    Login screen #2 + firebase

--- src/app/autenticacao/page.tsx
+++ src/app/autenticacao/page.tsx
@@ -1,33 +1,53 @@
 'use client'
 
 import AuthInput from '@/components/auth/AuthInput'
-import Image from 'next/image'
+import { IconeAtencao } from '@/components/icons'
 import { useState } from 'react'
 
 export default function Autenticacao() {
   const [modo, setModo] = useState<'login' | 'cadastro'>('login')
   const [email, setEmail] = useState('')
   const [senha, setSenha] = useState('')
+  const [erro, setErro] = useState<string | null>(null)
+
+  function exibirErro(msg: string, tempo = 5) {
+    setErro(msg)
+    setTimeout(() => setErro(null), tempo * 1000)
+  }
 
   function submeter() {
     if (modo === 'login') {
       console.log('Login')
+      exibirErro('Ocorreu um erro no login')
     } else {
       console.log('Cadastrar')
+      exibirErro('Ocorreu um erro no cadastro')
     }
   }
 
   return (
-    <div className='flex h-screen items-center justify-center'>
-      <picture className='hidden md:block md:w-1/2 lg:w-2/3'>
-        <img src="https://source.unsplash.com/random" alt="Imagem da tela de autenticação" className='h-screen w-full object-cover' />
+    <div className="flex h-screen items-center justify-center">
+      <picture className="hidden md:block md:w-1/2 lg:w-2/3">
+        <img
+          src="https://source.unsplash.com/random"
+          alt="Imagem da tela de autenticação"
+          className="h-screen w-full object-cover"
+        />
       </picture>
       <div className="m-10 w-full md:w-1/2 lg:w-1/3">
-        <h1 className="text-xl font-bold mb-5">
+        <h1 className="text-3xl font-bold mb-5">
           {modo === 'login'
             ? 'Entre com a sua conta'
             : 'Cadastre-se na plataforma'}
         </h1>
+
+        {erro && (
+          <div className="flex items-center bg-red-400 text-white py-3 px-5 my-2 border border-red-700 rounded-lg">
+            {IconeAtencao('w-7 h-7')}
+            <span className="ml-3">{erro}</span>
+          </div>
+        )}
+
         <AuthInput
           label="Email"
           tipo="email"
@@ -59,9 +79,27 @@ export default function Autenticacao() {
         </button>
 
         {modo === 'login' ? (
-          
+          <p className="mt-8">
+            Novo por aqui?
+            <a
+              onClick={() => setModo('cadastro')}
+              className="text-blue-500 hover:text-blue-700 font-semibold cursor-pointer"
+            >
+              {' '}
+              Crie uma conta gratuitamente
+            </a>
+          </p>
         ) : (
-          
+          <p className="mt-8">
+            Já faz parte da nossa comunidade?
+            <a
+              onClick={() => setModo('login')}
+              className="text-blue-500 hover:text-blue-700 font-semibold cursor-pointer"
+            >
+              {' '}
+              Entre com as suas credenciais
+            </a>
+          </p>
         )}
       </div>
     </div>
--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -68,7 +68,7 @@ export const IconeSair = (
   </svg>
 )
 
-export const IconeSol = (className = "w-6 h-6") => (
+export const IconeSol = (className = 'w-6 h-6') => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
@@ -85,7 +85,7 @@ export const IconeSol = (className = "w-6 h-6") => (
   </svg>
 )
 
-export const IconeLua = (className = "w-6 h-6") => (
+export const IconeLua = (className = 'w-6 h-6') => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
@@ -98,6 +98,23 @@ export const IconeLua = (className = "w-6 h-6") => (
       strokeLinecap="round"
       strokeLinejoin="round"
       d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+    />
+  </svg>
+)
+
+export const IconeAtencao = (className = 'w-6 h-6') => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className={className}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
     />
   </svg>
 )
